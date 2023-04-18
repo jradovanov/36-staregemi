56d2caad14e8e3987692f0a7d0925cefbaa8e688
Julian Radovanov <jradovanov@gmail.com>
2023-04-18 20:28:27 +0300

    36 strategemi
    36 strategies - ancient chinese wisdoms

---
+++ thirty_six_stategemi.py
@@ -0,0 +1,156 @@
+'''
+36 strategemi:
+    To achieve advantage and extract benefits
+# Strategy against a stronger opponent.
+
+1. Tricking the Emperor to swim across the sea - He who seeks to predict
+everything loses his vigilance.
+What you see during the day does not arouse suspicion.
+A clear day hides more than a dark night.
+To reveal all is to hide all.
+
+2. Besiege Wei to rescue Zhao - better divide your opponents than give
+them to be together. Attack where they are weak rather than
+where they fight back.
+
+3. Use foreign knives - With the enemy everything is clear,
+but with friends there is never certainty.
+Use the friends to remove the enemy without using your own powers.
+
+4. Calmly wait for the tired opponent - The strength that will
+defeat the adversary does not come from open opposition
+
+5. Take while on fire - If the enemy has suffered heavy losses,
+use the opportunity and take advantage for yourself.
+
+6. Raise a noise in the east, attack in the west - the spirit
+of the enemy and his army are confused. This is the favorable
+moment to attack.
+
+# Strategies at equal power
+
+7. Get something out of nothing - The art of cheating
+consists in first cheating and then not cheating. When the
+truth seems like a deception, it means that the truth is
+being hidden. First a small hidden truth, then a large
+hidden truth, and finally an attack.
+
+8. At first take offensive actions - If the opponent,
+convinced of his superiority, does not take anything in
+response, take advantage of his passivity
+
+9. Watch the fire from the opposite bank - let there be
+division and turmoil in the camp of the enemy. Stay away from
+it and wait for the crash to happen. Arguments, unpleasant looks,
+hatred is a sure sign that the enemy will destroy himself.
+
+10. Hide a knife behind a smile - Gain the opponent's trust,
+instill peace in him and then carry out your secret plans.
+Prepare everything properly, attack without hesitation,
+do not let the enemy come to his senses.
+
+11. Sacrifice the plum to save the peach - if the situation does
+not allow you to withdraw without losses, sacrifice the weak
+position to further strengthen the strong position.
+
+12. Lead the sheep with a light hand - Even the smallest weakness
+must be exploited. Even the smallest benefit should not be missed.
+The small weakness of the opponent, this is a small advantage for me.
+
+# Attacker Strategies
+
+13. Strike the grass to reveal the snakes - a lucky strike allows
+one to understand the true situation. Use the information,
+consider your actions and act decisively
+
+14. Use another to get your power back - when you can act for yourself,
+don't let them use you. When you can't do anything, try to take
+advantage of something. Use those who cannot act to serve you
+
+15. 
+
+Разбира се, мога да ви представя кратко всяка от "36 стратегеми" по-долу:
+
+1. Скрий се в шума - използвайте тълпата и анонимността,
+за да избягате от преследващите ви.
+2. Заплети ги в своята мрежа - използвайте манипулации,
+за да накарате противника ви да се разкрои.
+3. Насочи ги на изгубения път - измамете противника ви
+да поеме грешен път.
+4. Задай им капан - подгответе засада за противника си.
+5. Подкрепи малкото срещу големото - използвайте своята малка сила
+за да победите голяма сила.
+6. Удряй ги там, където са най-слаби - атакувайте противника
+в неговата най-слаба точка.
+7. Създайте повод за надежда, след това я унищожете -
+увеличете надеждата на противника си, а след това я разбийте.
+8. Прикрий се в мъглата, за да нанесеш удар - използвайте
+тайнствеността си, за да атакувате противника си изненадващо.
+9. Лови ги в капан - използвайте манипулации, за да накарате
+противника ви да попадне във вашия капан.
+10. Примамвай ги с изгубената миризма - използвайте някакъв вид
+примамка, за да накарате противника си да ви нападне.
+11. Престори се на мъртъв - правете се, че сте мъртъв,
+за да избегнете преследване или да изненадате противника си.
+12. Създай вражда между враговете си - подхранвайте конфликти
+между противници, за да отслабите техния съюз.
+13. Изтегли ги от земята - извлечете враговете от земята им,
+за да ги накарате да изгубят подкрепата на своятя земя.
+14. Насочи ги към крепостта, а след това я унищожи -
+накарайте противника ви да се насочи към една точка и след това
+я разрушете.
+15. Запали пожар зад гърба им - използвайте диверсия, за да отклоните
+вниманието на противника ви.
+16. Създайте проблем, за да намерите решение - използвайте един проблем,
+за да решите друг.
+17. Изберете края на тръбата - намерете начин да се измъкнете
+от затруднение, като търсите помощ от външен източник.
+18. Разбий ги с бездействие - избегнете директни сблъсъци с
+противника ви, като ги държите заети и ги карате да губят време.
+19. Направи си път, дори да пробиеш стената с глава - постоянно
+настоявайте, докато намерите начин да постигнете целта си.
+20. Закопай се и чакай - избегнете пряк сблъсък с противника ви,
+като се скриете и го накарате да се движи по вашите условия.
+21. Изостави утъпканите пътища, нападай по неутъпканите - нападайте
+противника ви по неговата най-незащитена точка.
+22. Удари се в краката, за да избегнеш гнева на главата - жертвайте
+нещо малко, за да избегнете по-големи загуби.
+23. тъпченето на тревата носи змии - внимавайте да не причините
+ненужни проблеми, като разкривате тайни и скрити намерения.
+24. Избегни контакта, когато си слаб - избягвайте директен конфликт,
+когато сте в слаба позиция.
+25. Използвайте противника ви за да си създадете път - използвайте
+мощта на противника ви, за да постигнете своите цели.
+26. Заложи всичко в една карта - рискувайте всичко за да
+постигнете голяма печалба.
+27. Планирай, докато действаш - продължете да планирате,
+докато сте в движение, за да можете да се адаптирате към
+промените в ситуацията.
+28. Остави да изтече времето - изчакайте, докато противникът ви сам
+се изложи и направи грешки.
+29. Изтънчеността е предвестник на несигурност - внимавайте с твърде
+изтънчени трикове, които могат да подведат.
+30. Заложи на неочакваното - направете нещо неочаквано, за да
+обърнете ситуацията във ваша полза.
+31. Големите дървета хващат повече вятър - бъдете внимателни,
+когато сте мощен играч, защото има повече вероятност да бъдете
+цел на атака.
+32. Когато сте в затруднение, отворете път за бягство - винаги имайте
+план за измъкване, когато се оказвате в затруднение.
+33. Не оставяй следи - използвайте трикове, за да затрудните
+проследяването ви.
+34. Планирайте за дългосрочния успех, докато се борите в
+краткосрочната битка - мислете за дългосрочната стратегия,
+докато се борите в текущата ситуация.
+35. Следвайте пътя на най-малкия съпротивлен - изберете пътя,
+който изисква най-малко усилия, за да постигнете целта си.
+36. Затисни ги в тясна долина - накарайте противника ви да се
+окаже в затруднено място, където няма много опции за действие.
+
+Тези стратегии могат да бъдат приложени в различни ситуации - от
+бизнеса до военните действия и междуличностни отношения.
+Важно е да се има предвид, че те могат да бъдат използвани
+за постигане на цели, но трябва да се използват с умереност
+и със здрав разум.
+
+'''
